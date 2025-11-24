99bf2e6b594e0803dd17781045b8a59168c49dbe
valmichi <deconwaypato@gmail.com>
2025-11-24 10:46:19 -0600

    usuario

--- src/app/vistas/usuario/usuario.component.ts
+++ src/app/vistas/usuario/usuario.component.ts
@@ -1,56 +1,85 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router} from '@angular/router'
 
 @Component({
   selector: 'app-usuario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
 export class UsuarioComponent {
 
-  currentSlide = 0;
+  constructor(private router: Router) {}
 
-  //Lista de salones simulada (cambiar con backend)
-  salones = [
+ busqueda = '';
+ filtroCapacidad = '';
+
+ salones = [
     {
-      nombre: 'Salón Flores',
-      img: '/salon1.jpg',
-      desc: 'Un salón elegante ideal para juntas ejecutivas y conferencias.'
+      id: 1,
+    nombre: 'Salón Montecarlo',
+    capacidad: 150,
+    ubicacion: 'Col. Centro',
+    img: '/salon1.jpg'
     },
     {
-      nombre: 'Salón Diamante',
-      img: '/salon2.jpg',
-      desc: 'Un salón elegante ideal para juntas ejecutivas y conferencias.'
+      id: 2,
+    nombre: 'Salón Imperial',
+    capacidad: 250,
+    ubicacion: 'Col. Reforma',
+    img: '/salon2.jpg'
     },
     {
-      nombre: 'Salón Buenavista',
-      img: '/salon3.jpg',
-      desc: 'Un salón elegante ideal para juntas ejecutivas y conferencias.'
+      id: 3,
+    nombre: 'Salón Diamante',
+    capacidad: 350,
+    ubicacion: 'Col. Jardines',
+    img: '/salon3.jpg'
     }
-  ];
+    ];
 
-  //Logica del carrusel
-  nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.salones.length;
-  }
 
-  prevSlide() {
-    this.currentSlide =
-      (this.currentSlide - 1 + this.salones.length) % this.salones.length;
-  }
+    salonesFiltrados = [...this.salones];
 
-  /** MODAL DE DETALLES */
-  modalAbierto = false;
-  salonSeleccionado: any = null;
+
+filtrar() {
+  const termino = this.removeAccents(this.busqueda);
+
+  this.salonesFiltrados = this.salones.filter(s => {
+
+    // Normalizamos el nombre del salón
+    const nombreNormalizado = this.removeAccents(s.nombre);
+
+    // Buscar sin acentos ni mayúsculas
+    const coincideBusqueda =
+      this.busqueda === '' || nombreNormalizado.includes(termino);
+
+    // Filtrado por capacidad
+    const coincideCapacidad = (() => {
+      if (!this.filtroCapacidad) return true;
+      const [min, max] = this.filtroCapacidad.split('-').map(Number);
+      return s.capacidad >= min && s.capacidad <= max;
+    })();
+
+    return coincideBusqueda && coincideCapacidad;
+  });
+}
 
   verDetalles(salon: any) {
-    this.salonSeleccionado = salon;
-    this.modalAbierto = true;
+      this.router.navigate(['/detalle-salon', salon.id]);
   }
 
-  cerrarModal() {
-    this.modalAbierto = false;
-    this.salonSeleccionado = null;
-  }
+    logout() {
+    console.log('Cerrar sesión');
+    }
+
+removeAccents(text: string): string {
+    return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // elimina acentos
+    .toLowerCase();
+}
+
 }
