b6bec8993f8a4c128f63dfbdf176ddfd0c76b589
valmichi <deconwaypato@gmail.com>
2025-12-06 17:06:23 -0600

    también para el admin

--- src/app/vistas/admin/admin.component.ts
+++ src/app/vistas/admin/admin.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { SalonInfoComponent } from "./salon-info/salon-info.component";
+import { PropietarioService } from '../../servicios/propietario.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  standalone: true,
+  imports: [SalonInfoComponent, CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  salones: any[] = [];
+  mostrarForm = false;
+  salonSeleccionado: any = null;
 
+  constructor(private propietarioService: PropietarioService) {}
+
+  ngOnInit() {
+    this.recargarSalones();
+  }
+
+  recargarSalones() {
+    this.salones = this.propietarioService.getSalones(); // simulación de backend
+  }
+
+  nuevoSalon() {
+    this.salonSeleccionado = null;
+    this.mostrarForm = true;
+  }
+
+  editarSalon(salon: any) {
+    this.salonSeleccionado = salon;
+    this.mostrarForm = true;
+  }
+
+  cerrarFormulario() {
+    this.mostrarForm = false;
+  }
 }
